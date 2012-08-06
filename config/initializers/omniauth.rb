d86725b0e40c686a35832f082f72cb91a5bf4ebf
Aurelien DEHAY <aurelien@dehay.info>
2012-08-06 22:00:11 +0200

    #

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # https://dev.twitter.com/apps
   provider :browser_id
   #provider :google, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-  provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :openid, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY']
 end
