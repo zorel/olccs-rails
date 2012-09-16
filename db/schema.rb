6f075dd30f4cde93834f1ba77045457e91201349
Aurelien DEHAY <aurelien@dehay.info>
2012-09-16 19:16:37 +0200

    Passage au session store BDD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912133615) do
+ActiveRecord::Schema.define(:version => 20120916164659) do
 
   create_table "links", :force => true do |t|
     t.text     "href"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(:version => 20120912133615) do
     t.datetime "updated_at", :null => false
     t.integer  "tribune_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "tribunes", :force => true do |t|
     t.text     "name"
