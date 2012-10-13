3217d599b9daab5d89ac4a37159fc0cbf5dbbe89
Aurelien DEHAY <aurelien@dehay.info>
2012-10-13 18:05:50 +0200

    modifs prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Olccs::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=43200"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
---
+++ config/initializers/filters.rb
@@ -0,0 +1,1 @@
+require "olccs_plugins_manager.rb"
