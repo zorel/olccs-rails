79afa2c3dd48f44a5c83d9eaf9b72440d3cd2d6f
Aurelien DEHAY <aurelien@dehay.info>
2012-10-13 16:14:20 +0200

    Show search some love, #1

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,6 +17,11 @@ class Post < ActiveRecord::Base
 
   end
 
+  def num_page(order = :id)
+    position = Post.where("#{order} >= ?", self.send(order)).count
+    (position.to_f/150).ceil
+  end
+
   private
   def update_message
 
